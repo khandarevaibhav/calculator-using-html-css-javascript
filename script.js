04746c5d5ab678e8445082a2f1081673d0f5e1af
vaibhav <vpkhandare2000@gmail.com>
2023-01-10 18:54:14 +0530

    added scientific calci

--- script.js
+++ script.js
@@ -9,14 +9,52 @@ for(i of  btn){
         btnval=e.target.innerText;
         screen.value+=btnval;
     });
+function sin(){
+    screen.value=Math.sin(screen.value);
+    }
+function cos(){
+    screen.value=Math.cos(screen.value);
+}
+function tan(){
+    screen.value=Math.tan(screen.value);
+}
+function pow(){
+    screen.value=Math.pow(screen.value, 2);
+}
+function pi(){
+    screen.value=3.14159265359;
+}
+function sqrt() {
+    screen.value = Math.sqrt(screen.value, 2);
+}
+function log(){
+    screen.value=Math.log(screen.value);
+}
+function e(){
+    screen.value=2.7182818284679;
+}
+function fact(){
+    var j, num, m;
+    m=1;
+    num=screen.value;
+    if(num<0){
+        console.log("factorial does not exit");
 
-let history = document.getElementById("history");
-    function calculation(y){
-        /* we created p element*/
-       val=document.createElement('p');
-       data=y+"="+eval(y);
-       val.innerText=data;
-       history.appendChild(val);
-       return eval(y);
+    }
+    else if(num==0){
+        console.log("1");
+    }
+    else{
+        for(j=1; j<=num; j++){
+            m=m*j;
+        }
+
+        screen.value=m;
+
+    }
+
+}
+function backspc() {
+    screen.value = screen.value.substr(0, screen.value.length - 1);
 }
 }
