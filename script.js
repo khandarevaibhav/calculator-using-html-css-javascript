5d37649a401b7fa149e375ea08b140b45bb5a66a
vaibhav <vpkhandare2000@gmail.com>
2023-01-10 18:36:43 +0530

    added history in calculator

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var screen=document.querySelector('#screen');
 var btn = document.querySelectorAll('.btn');
 
+
 /* to take value of button we are using a for loop */
 for(i of  btn){
     i.addEventListener('click', (e)=>{
@@ -8,4 +9,14 @@ for(i of  btn){
         btnval=e.target.innerText;
         screen.value+=btnval;
     });
+
+let history = document.getElementById("history");
+    function calculation(y){
+        /* we created p element*/
+       val=document.createElement('p');
+       data=y+"="+eval(y);
+       val.innerText=data;
+       history.appendChild(val);
+       return eval(y);
+}
 }
